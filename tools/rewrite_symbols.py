1f1cdecce2ecc65fa1c2ec928750cd224e3e112c
brainWave4 <pikadon92+1@gmail.com>
2026-09-26 22:56:50 +0800

    Add Bank Files to Read

--- tools/rewrite_symbols.py
+++ tools/rewrite_symbols.py
@@ -38,3 +38,29 @@ FILEPATH_SYM = pathlib.Path("recomp") / "symbols.toml"
 
 with open(FILEPATH_SYM, "w") as file_sym:
     write_intro(file_sym)
+
+    DIR_BANKS = pathlib.Path("recomp") / "disassembly" / "src"
+    ASM_EXT = ".asm"
+
+    FILES_BANKS = {
+        "field/field-main": 0xc0,
+        "btlg/btlgfx-main": 0xc1,
+        "battle/battle-main": 0xc2,
+        "menu/menu-main": 0xc2,
+        "cutscene/cutscene-main": 0xc3,
+        "sound/sound-main": 0xc4
+    }
+
+    for key in FILES_BANKS:
+        file_sym.write(textwrap.dedent(f'''
+            # From {key}
+        '''))
+        
+        filepath_bank = DIR_BANKS / (key + ASM_EXT)
+
+        with open(filepath_bank) as file_bank:
+            for line_bank in file_bank:
+                if ":" in line_bank:
+                    print(line_bank)
+            
+            file_bank.close()
